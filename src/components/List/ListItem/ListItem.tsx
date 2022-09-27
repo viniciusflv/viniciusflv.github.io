0d3cf3fd5fa8c98f875f201eb402938e943569ea
Vinícius Victorino <viniciusflv@gmail.com>
2022-09-27 18:10:54 -0300

    docs(mermaid): subgraphs and interactions

--- src/components/List/ListItem/ListItem.tsx
+++ src/components/List/ListItem/ListItem.tsx
@@ -1,15 +1,19 @@
 export const ListItem = ({ depth, children }: any) => {
   let node = <li className="truncate">{children}</li>;
 
-  if (depth > 1) {
-    for (const _ of Array(depth)) {
-      node = (
-        <ul>
-          <li className="pl-4">{node}</li>
-        </ul>
-      );
+  if (depth < 5) {
+    if (depth > 1) {
+      for (const _ of Array(depth)) {
+        node = (
+          <ul>
+            <li className="pl-4">{node}</li>
+          </ul>
+        );
+      }
     }
+
+    return node;
   }
 
-  return node;
+  return null;
 };
