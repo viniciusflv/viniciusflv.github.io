236fff457093933ed988aa9d1008b64ea0a9ae3e
Vinícius Victorino <viniciusflv@gmail.com>
2022-10-02 19:18:22 -0300

    docs(mermaid): git graph

--- src/components/List/ListItem/ListItem.tsx
+++ src/components/List/ListItem/ListItem.tsx
@@ -1,19 +1,15 @@
 export const ListItem = ({ depth, children }: any) => {
   let node = <li className="truncate">{children}</li>;
 
-  if (depth < 5) {
-    if (depth > 1) {
-      for (const _ of Array(depth)) {
-        node = (
-          <ul>
-            <li className="pl-4">{node}</li>
-          </ul>
-        );
-      }
+  if (depth > 1) {
+    for (const _ of Array(depth)) {
+      node = (
+        <ul>
+          <li className="pl-4">{node}</li>
+        </ul>
+      );
     }
-
-    return node;
   }
 
-  return null;
+  return node;
 };
