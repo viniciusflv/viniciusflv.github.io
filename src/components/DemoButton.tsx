f6b10c9d7a8b567a144c36f4b478d766d76cd952
Vinícius Victorino <viniciusflv@gmail.com>
2022-10-12 18:59:52 -0300

    docs(frameworks): partial hydration example

---
+++ src/components/DemoButton.tsx
@@ -0,0 +1,26 @@
+import { useReducer } from 'react';
+
+export const DemoButton = () => {
+  const [active, toggleActive] = useReducer((prev) => {
+    return !prev;
+  }, false);
+
+  return (
+    <button
+      className={`
+      cursor-pointer
+      py-5
+      px-10
+      w-full
+      max-w-64
+      rounded
+      truncate
+      text-white
+      ${active ? 'bg-dark-comment' : 'bg-dark-currentLine'}
+      `}
+      onClick={toggleActive}
+    >
+      DemoButton
+    </button>
+  );
+};
