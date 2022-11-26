cab10b1297069586615e751e0e5eb9e36a2914fd
Vinícius Victorino <viniciusflv@gmail.com>
2022-11-26 19:00:19 -0300

    refactor(css): header

---
+++ src/assets/svg/home.ts
@@ -0,0 +1,19 @@
+export const home = {
+  tag: 'svg',
+  props: {
+    'aria-hidden': 'true',
+    'data-testid': 'HomeIcon',
+    tabindex: '-1',
+    viewBox: '0 0 24 24',
+    className:
+      'MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiSvgIcon-fontSizeLarge css-1shn170',
+  },
+  children: [
+    {
+      tag: 'path',
+      props: {
+        d: 'M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z',
+      },
+    },
+  ],
+};
