7595efdf35e7dca81f26908356c00d80c2046e8d
Vinícius Victorino <viniciusflv@gmail.com>
2022-11-27 11:51:46 -0300

    refactor(css): menu item

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ const plugin = require('tailwindcss/plugin');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
