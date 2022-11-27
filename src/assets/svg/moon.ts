1ecbdee963b6a971f4a52d868956ccb80cc28a79
Vinícius Victorino <viniciusflv@gmail.com>
2022-11-27 17:32:38 -0300

    refactor(css): toggle theme

---
+++ src/assets/svg/moon.ts
@@ -0,0 +1,19 @@
+export const moon = {
+  tag: 'svg',
+  props: {
+    'aria-hidden': 'true',
+    'data-testid': 'DarkModeIcon',
+    tabindex: '-1',
+    viewBox: '0 0 24 24',
+    className:
+      'MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiSvgIcon-fontSizeLarge css-1shn170',
+  },
+  children: [
+    {
+      tag: 'path',
+      props: {
+        d: 'M12 3a9 9 0 1 0 9 9c0-.46-.04-.92-.1-1.36a5.389 5.389 0 0 1-4.4 2.26 5.403 5.403 0 0 1-3.14-9.8c-.44-.06-.9-.1-1.36-.1z',
+      },
+    },
+  ],
+};
