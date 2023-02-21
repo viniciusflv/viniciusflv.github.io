d6addccf603a7a174c4d8b0ae28c7384c64c407c
Vinícius Victorino <viniciusflv@gmail.com>
2023-02-21 19:26:03 -0300

    refactor: astro v2

---
+++ src/consts.ts
@@ -0,0 +1,36 @@
+export const TRANSLATIONS = {
+  en: {
+    HEADER_MENU_ITEM_HOME: `Home`,
+    HEADER_MENU_ITEM_POSTS: `Posts`,
+    HEADER_MENU_ITEM_ABOUT: `About`,
+    HEADER_MENU_ITEM_TOGGLE_THEME: `Theme`,
+    HEADER_MENU_ITEM_TOGGLE_THEME_DARK_ALT: `Dark`,
+    HEADER_MENU_ITEM_TOGGLE_THEME_LIGHT_ALT: `Light`,
+    HOME_HERO_H1: `Welcome to my personal website!`,
+    HOME_HERO_H2: `Here I talk about my experiences and technologies, that I'm passionate about.`,
+    HOME_HERO_BUTTON_PRIMARY: `My posts`,
+    HOME_HERO_BUTTON_SECONDARY: `About me`,
+    HOME_HERO_IMAGE_ALT: `Me as a Rick and Morty Character`,
+    HOME_POSTS_TITLE: `Last Published Posts`,
+    HOME_CATEGORY_TITLE: `By Category`,
+    POST_PREVIEW_LINK: `Continue reading`,
+  },
+  'pt-BR': {
+    HEADER_MENU_ITEM_HOME: `Início`,
+    HEADER_MENU_ITEM_POSTS: `Postagens`,
+    HEADER_MENU_ITEM_ABOUT: `Sobre`,
+    HEADER_MENU_ITEM_TOGGLE_THEME: `Tema`,
+    HEADER_MENU_ITEM_TOGGLE_THEME_DARK_ALT: `Escuro`,
+    HEADER_MENU_ITEM_TOGGLE_THEME_LIGHT_ALT: `Claro`,
+    HOME_HERO_H1: `Bem vindo ao meu site pessoal!`,
+    HOME_HERO_H2: `Aqui eu falo sobre minhas experiências e as tecnologias que sou apaixonado.`,
+    HOME_HERO_BUTTON_PRIMARY: `Minhas postagens`,
+    HOME_HERO_BUTTON_SECONDARY: `Sobre mim`,
+    HOME_HERO_IMAGE_ALT: `Eu como um personagem de Rick and Morty`,
+    HOME_POSTS_TITLE: `Posts publicados recentemente`,
+    HOME_CATEGORY_TITLE: `Por categoria`,
+    POST_PREVIEW_LINK: `Continue lendo`,
+  },
+};
+
+export const KNOWN_LANGUAGES = Object.keys(TRANSLATIONS);
--- src/util.ts
+++ src/util.ts
@@ -1,12 +1,51 @@
-import type { MarkdownInstance } from 'astro';
+import type { MarkdownHeading, MarkdownInstance } from 'astro';
+
+import { TRANSLATIONS } from './consts';
 
 export function getLanguageFromURL(pathname: string) {
-  const { lang, country }: any = {
+  const { lang, country }: Record<string, string> = {
     ...(/^\/(?<lang>\w+)/.exec(pathname)?.groups ?? {}),
     ...(/^\/(?<lang>\w+)-(?<country>\w+)/.exec(pathname)?.groups ?? {}),
   };
 
   return lang || country ? (country ? `${lang}-${country}` : lang) : 'en';
+}
+
+type Translations = typeof TRANSLATIONS;
+type TranslationsLangs = keyof Translations;
+type TranslationsKeys = keyof Translations[TranslationsLangs];
+
+export function getTranslation(lang: string, key: TranslationsKeys) {
+  return TRANSLATIONS[lang as TranslationsLangs][key];
+}
+
+export type NestedHeading = MarkdownHeading & {
+  children: NestedHeading[];
+};
+
+export function unflattenHeading(headings: MarkdownHeading[]): NestedHeading[] {
+  let root: NestedHeading = { depth: 0, slug: '', text: '', children: [] };
+  let stack: NestedHeading[] = [root];
+
+  for (let i = 0; i < headings.length; i++) {
+    const heading = headings[i];
+    const node: NestedHeading = {
+      depth: heading.depth,
+      slug: heading.slug,
+      text: heading.text,
+      children: [],
+    };
+
+    while (stack.length > heading.depth) {
+      stack.pop();
+    }
+
+    const parent = stack[stack.length - 1];
+    parent.children.push(node);
+    stack.push(node);
+  }
+
+  return root.children;
 }
 
 export const groupPagesByLang = <
