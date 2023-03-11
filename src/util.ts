404e1835fbe91f96776af908479ac07995587bb5
Vinícius Victorino <viniciusflv@gmail.com>
2023-03-11 18:31:14 -0300

    chore: update about

--- src/util.ts
+++ src/util.ts
@@ -2,13 +2,52 @@ import type { MarkdownHeading, MarkdownInstance } from 'astro';
 
 import { TRANSLATIONS } from './consts';
 
+export function dateFormat(
+  date: number | Date,
+  lang: TranslationsLangs = 'en',
+) {
+  const dateFormatter = new Intl.DateTimeFormat(lang);
+  return dateFormatter.format(date);
+}
+
+function getPartsValue(parts: Intl.RelativeTimeFormatPart[]) {
+  const number = parts[parts.length - 2];
+  const time = parts[parts.length - 1];
+
+  return number?.value + time?.value;
+}
+
+export function relativeDate(
+  startDate: number | Date | string,
+  endDate: number | Date | string,
+  lang: TranslationsLangs = 'en',
+) {
+  const relativeTime = new Intl.RelativeTimeFormat(lang, {
+    style: 'long',
+  });
+  const startYear = new Date(startDate).getFullYear();
+  const endYear = new Date(endDate).getFullYear();
+  if (startYear - endYear <= 0) {
+    return getPartsValue(
+      relativeTime.formatToParts(startYear - endYear, 'years'),
+    );
+  }
+  const startMonth = new Date(startDate).getMonth();
+  const endMonth = new Date(endDate).getMonth();
+  return getPartsValue(
+    relativeTime.formatToParts(startMonth - endMonth, 'months'),
+  );
+}
+
 export function getLanguageFromURL(pathname: string) {
   const { lang, country }: Record<string, string> = {
     ...(/^\/(?<lang>\w+)/.exec(pathname)?.groups ?? {}),
     ...(/^\/(?<lang>\w+)-(?<country>\w+)/.exec(pathname)?.groups ?? {}),
   };
 
-  return lang || country ? (country ? `${lang}-${country}` : lang) : 'en';
+  return (
+    lang || country ? (country ? `${lang}-${country}` : lang) : 'en'
+  ) as TranslationsLangs;
 }
 
 type Translations = typeof TRANSLATIONS;
@@ -19,9 +58,11 @@ export function getTranslation(lang: string, key: TranslationsKeys) {
   return TRANSLATIONS[lang as TranslationsLangs][key];
 }
 
-export type NestedHeading = MarkdownHeading & {
-  children: NestedHeading[];
-};
+export type NestedHeading = Partial<
+  MarkdownHeading & {
+    children: NestedHeading[];
+  }
+>;
 
 export function unflattenHeading(headings: MarkdownHeading[]): NestedHeading[] {
   let root: NestedHeading = { depth: 0, slug: '', text: '', children: [] };
@@ -41,11 +82,11 @@ export function unflattenHeading(headings: MarkdownHeading[]): NestedHeading[] {
     }
 
     const parent = stack[stack.length - 1];
-    parent.children.push(node);
+    parent.children?.push(node);
     stack.push(node);
   }
 
-  return root.children;
+  return root?.children as any;
 }
 
 export const groupPagesByLang = <
