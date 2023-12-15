9562ac097d7b6845a73a42d0402e4e9652c7495d
Vinícius Victorino <viniciusflv@gmail.com>
2023-12-15 16:59:47 -0300

    chore: add prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  useTabs: false,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderGroupNamespaceSpecifiers: true,
+  importOrderCaseInsensitive: true,
+  importOrder: ['^react', '<THIRD_PARTY_MODULES>', '^@\\/', '^..\\/', '^.\\/'],
+};
